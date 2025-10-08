64955328759e0e20217ecba059b5837a50304364
Filipe Marques <filipemarquespascoal@hotmail.com>
2025-10-07 23:57:50 -0300

    first structure added

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,7 @@
+export default function Carousel() {
+    return (
+        <div className="flex w-[400px] lg:w-[1200px] lg:h-[520px] rounded-3xl bg-black text-white flex items-center justify-center shadow-lg mx-auto mt-20">
+            Carousel
+        </div>
+    )
+}
